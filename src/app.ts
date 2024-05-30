b262872d9bc7619f54f3330cc1da1e1fcd6e25ce
Guilherme <guilherme1984.araujo@gmail.com>
2024-05-30 15:36:27 -0300

    add comentário

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,16 @@ function createApp(){
     
     app.use(express.json())
     app.use("/api",router)
+
+    // const corsOptions = {
+    //     origin: 'http://Guilhermao.sistem.com', se vender precisa colocar entre vetor ex.: ['http://Guilhermao.sistem.com', 'http://paizao.sistem.com']
+    //     methods: ["GET"]
+    // }
+
     app.use(cors())
 
     return app
 }
 
 export default createApp
+//cheguei nesse ponto dia 30/052024 
